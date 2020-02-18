e7a0bcd4cbe1a81a62fa24405820da632cf552b6
Fabian Siebert <fabian.siebert@aboutyou.com>
2020-02-18 08:37:54 +0100

    task(docker-compose.yml/entrypoint.sh): Change restart to unless-stopped in docker-compose and add new java arguments to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "APP_UID: ${APP_UID}"
 echo "APP_GID: ${APP_GID}"
 echo "APP_MEMORY: ${APP_MEMORY}"
 
-APP_ARGS="-XX:ParallelGCThreads=7 -Xmx${APP_MEMORY} -Xms${APP_MEMORY} -jar /usr/share/java/papermc.jar"
+APP_ARGS="-Xms${APP_MEMORY} -Xmx${APP_MEMORY} -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -jar /usr/share/java/papermc.jar nogui"
 
 echo "minecraft:x:${APP_UID}:${APP_GID}:The Docker User:/home/minecraft:/bin/sh" >> /etc/passwd
 echo "minecraft:x:${APP_GID}:" >> /etc/group
